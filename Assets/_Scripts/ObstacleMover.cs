5ed0834ef9413cebc828f7c3b1933a4510aeb56b
Stedd <tor.andre.paulsen@gmail.com>
2021-09-12 18:27:41 +0200

    Protection agains NaN

--- Assets/_Scripts/ObstacleMover.cs
+++ Assets/_Scripts/ObstacleMover.cs
@@ -22,6 +22,8 @@ public class ObstacleMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(oscilationPeriod <= Mathf.Epsilon) { return; }
+
         movementFactor = 0.5f+(0.5f*Mathf.Sin(((Time.time/(oscilationPeriod+oscilationPeriodOffset))* Mathf.PI*2) - Mathf.PI / 2));
         transform.position = startPos + movementVector * movementFactor;
     }
